6a5073fb5fc3a63c0862d7ad79ec55cdf7eb6d98
Dawn Budge <dawn.budge@ft.com>
2018-09-19 15:35:09 +0100

    Better

--- nlp.js
+++ nlp.js
@@ -1,3 +1,6 @@
+// CAVEAT there will be nicer ways of doing this, time limits for a hackday
+// e.g. might have been nicer to use Map or Set and have a reference to link chunks to original json array items
+
 const NLU = require('watson-developer-cloud/natural-language-understanding/v1.js');
 require('dotenv').config();
 
@@ -35,34 +38,56 @@ const analyze = (text) => {
 	});
 };
 
-// there will be nicer ways of doing this, time limits for a hackday
-const splitChunks = (vttAsJson) => {
-	const chunks = [];
+
+const getText = (vttAsJson) => {
 	const justText = vttAsJson.map(items => {
 		return items.part;
 	});
+	return justText;
+}
+
+const splitChunks = (vttText) => {
+	const chunks = [];
 	do {
-		const chunkParts = justText.splice(0, 10);
+		const chunkParts = vttText.splice(0, 10);
 		const chunk = chunkParts.join(' ');
 		chunks.push(chunk);
-	} while (justText.length);
+	} while (vttText.length);
 	return chunks;
 };
 
-module.exports = async (vttAsJson) => {
-	const chunks = splitChunks(vttAsJson);
-	chunks.forEach(async (chunk) => {
-		try {
-			const analysedText = await analyze(chunk);
-			if (analysedText && analysedText.categories) {
-				const topCategoryName = topCategory(analysedText.categories);
-				//now need to add this back to original part
-			}
+// const addCategoryToVTTJson = ({chunk, topCategoryName, vttText}) => {
+const addCategoryToVTTJson = (vttAsJson, topCategoryName, index) => {
+	const start = index * 10;
+	const end = start + 10;
+	for (let i = start; i < end; i += 1) {
+		if (vttAsJson[i]) {
+			vttAsJson[i].topic = topCategoryName;
 		}
-		catch (err) {
+	}
+	return vttAsJson;
+};
+
+module.exports = async (vttAsJson) => {
+	const vttText = getText(vttAsJson);
+	const chunks = splitChunks(vttText);
+	//cant' know when we have the last one done. promise all?
+	const analysedChunks = chunks.map(chunk => {
+		return analyze(chunk);
+	});
+	return Promise.all(analysedChunks)
+		.then(analysedTexts => {
+			analysedTexts.forEach((analysedText, index) => {
+				if (analysedText && analysedText.categories) {
+					const topCategoryName = topCategory(analysedText.categories);
+					addCategoryToVTTJson(vttAsJson, topCategoryName, index);
+				}
+			})
+			console.log(vttAsJson)
+			return vttAsJson;
+		})
+		.catch (err => {
 			console.error(err);
 			return new Error(err);
-		}
 	});
-	return 'asf';
 };
